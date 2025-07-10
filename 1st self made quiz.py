c0f0e7589c5fe59e631417e17bcf8f1eb84cd82a
Gurnoor Singh <gurnoorssd@gmail.com>
2025-07-10 21:53:49 +0530

    Add files via upload

---
+++ 1st self made quiz.py
@@ -0,0 +1,22 @@
+Python 3.12.8 (tags/v3.12.8:2dc476b, Dec  3 2024, 19:30:04) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:/Users/Lenovo/OneDrive/Else/Desktop/punjab quiz.py ========
+How many rivers were there in late Punjab?
+ a. 1   b. 4    c. 5    d. 7
+c
+correct answer
+
+Who was the last maharaja of Punjab?
+ a. Maharaja Duleep Singh     b. Maharaja Gurnoor Singh    c. Majaraja Joyi Singh    d. Maharaja Mummy Daddy
+a
+correct answer
+Isaac Newton gave us the theory of ______ 
+ a. Relativity     b. Blackholes   c. Genral relativity    d. Gravity
+a
+wrong answer
+
+Isaac Newton gave us the theory of ______ 
+ a. Relativity     b. Blackholes   c. Genral relativity    d. Gravity
+d
+correct answer
